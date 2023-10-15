033e58918e4abaaf99b59003a7c856521a4c48f8
Hicham Chaguer <hchaguer@e2r11p5.1337.ma>
2023-10-16 00:03:12 +0100

    ex01

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -1,34 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   contact.hpp                                        :+:      :+:    :+:   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hchaguer <hchaguer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 15:40:08 by hchaguer          #+#    #+#             */
-/*   Updated: 2023/10/15 20:57:16 by hchaguer         ###   ########.fr       */
+/*   Created: 2023/10/15 16:51:15 by hchaguer          #+#    #+#             */
+/*   Updated: 2023/10/15 20:59:25 by hchaguer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CONTACT_HPP
-#define CONTACT_HPP
-#include <string>
-#include <iostream>
+#include "contact.hpp"
 
-using namespace std;
-
-class Contact {
-    
-    private :
-        int     num_phone;
-        string  fisrtName;
-        string  lastName;
-        string  nickName;
-        string  darkest_secret;
-
-    public :
-
-        Contact(int Num, string First, string Last, string Nick, string Dark);
-};
-
-#endif
+Contact::Contact(int Num, string First, string Last, string Nick, string Dark) : num_phone(Num), fisrtName(First), 
+    lastName(Last),nickName(Nick), darkest_secret(Dark)
+{
+    // this->num_phone;
+    // this->fisrtName;
+    // this->lastName;
+    // this->nickName;
+    // this->darkest_secret;
+}
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,13 +6,40 @@
 /*   By: hchaguer <hchaguer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:23:34 by hchaguer          #+#    #+#             */
-/*   Updated: 2023/10/13 22:43:42 by hchaguer         ###   ########.fr       */
+/*   Updated: 2023/10/15 21:09:51 by hchaguer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+#include "contact.hpp"
 #include <iostream>
 #include <unistd.h>
+
+
+void fill_contact()
+{
+    int num;
+    string first, last, nick, dark;
+    
+    cout<< "num : ";
+    std::cin >> num ;
+     cout<< "first name : ";
+    std::cin >> first;
+     cout<< "last name : ";
+    std::cin >> last;
+     cout<< "nick Name : ";
+    std::cin >> nick;
+     cout<< "dark side : ";
+    std::cin >> dark;
+    
+    Contact contact(num, first, last, nick, dark);
+}
+
+void search(void)
+{
+    
+}
+
 
 int main()
 {
@@ -27,19 +54,22 @@ int main()
             
             cout<<" please enter one of the following command ! "<<endl;
             cout<<"{ ADD, SEARCH, EXIT }"<<endl;
+            cout<<endl;
             
         }
         if (str.compare("ADD") == 0)
-        {
-             cout<<"ADD"<<endl;
-        }
+            fill_contact();
+        
         if (str.compare("SEARCH") == 0)
         {
              cout<<"SEARCH"<<endl;
         }
         if (str.compare("EXIT") == 0)
         {
-             cout<<"EXIT"<<endl;
+            cout<< "EXIT PROGARM"<< std::endl;
+             exit(0);
         }
     }
 }
+
+
